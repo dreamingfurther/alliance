d24f2171ea99b2403e0ad18f14b6ae976e523b43
PivotalCommon <dreamingfurther@gmail.com>
2013-02-20 19:31:58 -0500

    Adds blank rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Alliance::Application.config.secret_token = 'e87ce0d0c7e72c9c86afa008a6e1e0f61bd17ca82cc31ec239aeb9dd5b9a7d653c01d615cb6ab7046417dcc29541d0904143dd22efa283945fc74c6787f0512c'
